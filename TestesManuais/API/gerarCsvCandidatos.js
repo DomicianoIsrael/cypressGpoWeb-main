dce5d753294f7d4a25a60e8be7926a24fe86ed6a
Israel Domiciano <121518682+DomicianoIsrael@users.noreply.github.com>
2023-06-19 11:21:20 -0300

    Atualização da geração

--- TestesManuais/API/gerarCsvCandidatos.js
+++ TestesManuais/API/gerarCsvCandidatos.js
@@ -1,5 +1,6 @@
 const faker = require('faker-br');
 const fs = require('fs');
+const readline = require('readline');
 
 // Função para formatar o nome removendo abreviações
 function formatarNome(nome) {
@@ -34,52 +35,63 @@ function gerarSenhaFixa() {
     return 'Cesgran123';
 }
 
-// Definir o número de linhas que deseja gerar
-const numeroDeLinhas = 30; // Altere para o número desejado
+// Função para gerar um código alfanumérico de 6 caracteres
+function gerarCodigoAleatorio() {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let codigo = '';
 
-// Verificar se o arquivo já existe
-const arquivoCSV = numeroDeLinhas + ' LinhasGeradas.csv';
-
-// Conjunto para armazenar os e-mails gerados
-const emailsGerados = new Set();
-
-for (let i = 0; i < numeroDeLinhas; i++) {
-    let email = faker.internet.email();
-
-    // Verificar se o e-mail já foi gerado
-    while (emailsGerados.has(email)) {
-        email = faker.internet.email();
+    for (let i = 0; i < 6; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        codigo += caracteres[indiceAleatorio];
     }
 
-    // Adicionar o e-mail ao conjunto de e-mails gerados
-    emailsGerados.add(email);
-
-    // Gerar dados aleatórios para cada campo (alguns campos não estão sendo utilizados, mas se for utilizar já está aqui)
-    const nome = gerarNomeCompletoSemErro();
-    const cpf = faker.br.cpf();
-    const nomeMae = gerarNomeCompletoSemErro();
-    const nomePai = gerarNomeCompletoSemErro();
-    const sobrenomeMae = faker.name.lastName();
-    const sobrenomePai = faker.name.lastName();
-    const nomeCompletoMae = `${nomeMae} ${sobrenomeMae}`;
-    const nomeCompletoPai = `${nomePai} ${sobrenomePai}`;
-    const telefone = faker.phone.phoneNumber().replace(/\D/g, '');
-    const dataNascimento = faker.date.past().toLocaleDateString('pt-BR');
-    const numeroInscricao = cpf;
-    const senha = gerarSenhaFixa();
-
-    // Montar a linha do CSV com os campos desejados
-    const linhaCSV = `${'Teste ' + nome},${cpf},${cpf},'${nomeMae} Teste','${nomePai} Teste',${email},${telefone},${dataNascimento},${numeroInscricao},${senha},semfoto.jpg\n`;
-
-    const linhaCSVComVirgulas = linhaCSV.replace(/,/g, ';');
-
-    if (fs.existsSync(arquivoCSV)) {
-        // Se o arquivo já existe, apenas adiciona a nova linha
-        fs.appendFileSync(arquivoCSV, linhaCSVComVirgulas);
-    } else {
-        // Se o arquivo não existe, cria o arquivo diretamente com a nova linha
-        fs.writeFileSync(arquivoCSV, linhaCSVComVirgulas);
-    }
+    return codigo;
 }
 
-console.log('Arquivo CSV gerado com sucesso!');
+
+// Criar uma interface de leitura do console
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Perguntar ao usuário quantas linhas deseja gerar
+rl.question('Quantas linhas você deseja gerar? ', (numeroDeLinhas) => {
+    // Fechar a interface de leitura do console
+    rl.close();
+
+    // Verificar se o arquivo já existe
+    const arquivoCSV = numeroDeLinhas + ' LinhasGeradas.csv';
+
+    // Conjunto para armazenar os códigos gerados
+    const codigosGerados = new Set();
+
+    for (let i = 0; i < numeroDeLinhas; i++) {
+        let codigo = gerarCodigoAleatorio();
+
+        // Verificar se o código já foi gerado
+        while (codigosGerados.has(codigo)) {
+            codigo = gerarCodigoAleatorio();
+        }
+
+        // Adicionar o código ao conjunto de códigos gerados
+        codigosGerados.add(codigo);
+
+        // Gerar dados aleatórios para cada campo
+        const nome = gerarNomeCompletoSemErro();
+        const senha = gerarSenhaFixa();
+
+        // Montar a linha do CSV com os campos desejados
+        const linhaCSV = `${nome};${codigo};${senha}\n`;
+
+        if (fs.existsSync(arquivoCSV)) {
+            // Se o arquivo já existe, apenas adiciona a nova linha
+            fs.appendFileSync(arquivoCSV, linhaCSV);
+        } else {
+            // Se o arquivo não existe, cria o arquivo diretamente com a nova linha
+            fs.writeFileSync(arquivoCSV, linhaCSV);
+        }
+    }
+
+    console.log('Arquivo CSV gerado com sucesso!');
+});
